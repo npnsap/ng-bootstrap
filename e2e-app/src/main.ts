159e6e9cada6abbe80e7d7cb77639917f7f2d4ed
npnsap <143161837+npnsap@users.noreply.github.com>
2023-12-20 06:16:27 +0000

    build: deleted all except dist folder

--- e2e-app/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { AppComponent } from './app/app.component';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { ROUTES } from './app/app.routes';
-
-bootstrapApplication(AppComponent, {
-	providers: [provideRouter(ROUTES, withHashLocation())],
-}).catch((err) => console.log(err));
